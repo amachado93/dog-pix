859c2576faee2773de6bb57b8e0890652e299dcf
Alex Machado <amachado@sva.edu>
2021-05-25 22:09:07 -0400

    Build basic dog app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import BreedList from './components/BreedList';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      dogs: []
+    }
+  }
+
+  fetchDog = async () => {
+    const response = await axios.get('https://dog.ceo/api/breeds/list/all');
+    const dogs = Object.keys(response.data.message);
+    /* if the key and the value are the same word...
+     this...
+     this.setState({ dogs: dogs }) 
+     is the same ast this: */
+    this.setState({ dogs })
+  }
+
+  fetchRandomDogPic = async (breed) => {
+    const response = await axios.get(`https://dog.ceo/api/breed/${breed}/images/random`);
+    this.setState({
+      randomDogPic: response.data.message
+    })
+  }
+
+  render(){
+    
+    return (
+      <div className="App">
+
+        <h1>Dog Pictures!</h1>
+        { this.state.randomDogPic && <img src={this.state.randomDogPic} alt="cute dog" /> }
+        <BreedList 
+        dogs={this.state.dogs} 
+        fetchDog={this.fetchDog} 
+        fetchRandomDogPic={this.fetchRandomDogPic}
+        />
+      
+      </div>
+    );
+  }
 }
 
 export default App;
